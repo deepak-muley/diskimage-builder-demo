40cc7308ae260517f7e4a74b8a73ab1d383b44d5
Deepak Muley <deepak.muley@gmail.com>
2019-06-24 22:40:04 -0700

    Update build-qcow2-image.sh

--- deepak-ubuntu/build-qcow2-image.sh
+++ deepak-ubuntu/build-qcow2-image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 disk-image-create --version
-
-export ELEMENTS_PATH=/home/deepak/workspace/sourcecode/disk-images/using-diskimage-builder
+export ELEMENTS_PATH=/home/deepak/diskimage-builder-demo
 disk-image-create -t qcow2 -o deepak-ubuntu.qcow2 deepak-ubuntu
 qemu-img convert -O vdi deepak-ubuntu.qcow2 deepak-ubuntu.vdi
